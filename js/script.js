d9e068ad65687a5f0c3abab202dfdb973f147dda
Simone <meto0002@stud.kea.dk>
2024-06-12 00:37:32 +0200

    copy portfolio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    hideSidebar();
+});
+// window.addEventListener("load", hideSidebar);
+
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex' 
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none' 
+}
